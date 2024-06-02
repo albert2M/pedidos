50ea1405f405a0d72d596341230d908daca79fdd
albert2M <gammalbert@gmail.com>
2024-06-02 22:50:03 +0200

    Anotaciones

--- src/components/title-component.js
+++ src/components/title-component.js
@@ -6,7 +6,15 @@ class Title extends HTMLElement {
       this.color= this.getAttribute("color")
       this.fontSize = this.getAttribute('font-size')
       this.title = this.getAttribute('title')
-      
+      /*Estas son las propiedades del componente.
+       Son variables (Ej: this.shadow) que se pueden
+      usar en cualquier parte del componente.
+      Por ejemplo en el caso de this.title = this.getAttribute('title)
+      Lo que se le está diciendo al componente es 
+      que coja el valor del atributo title que se le está pasando en el 
+      index.html al componente, lo guarde en la variable this.title (que por convención se le pone el mismo nombre del atributo ); 
+      el cual será mostrado por pantalla al ser un css en la funcion render*/ 
+ 
     }
   
     connectedCallback () {
@@ -19,7 +27,7 @@ class Title extends HTMLElement {
         <style>    
           h2 {
             font-family: "Lato", sans-serif;
-            font-size: ${this.fontSize};
+            font-size: ${this.fontSize};  /*${this.fontSize} Esto hace qu el atributo sea una variable capturable en el constructor */
             font-weight: 500;
             margin: 0;
             color: ${this.color};
