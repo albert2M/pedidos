b25a6937ec3442750a14b04eb1337c549ae5c17c
albert2M <gammalbert@gmail.com>
2024-08-01 13:59:27 +0200

    error 404

--- api/src/controllers/customer/auth-routes-controller.js
+++ api/src/controllers/customer/auth-routes-controller.js
@@ -6,3 +6,11 @@ exports.findAll = (req, res) => {
 
   res.status(200).send(routes)
 }
+exports.findAll = (req, res) => {
+  const routes = {
+    '/cliente/login': 'login.html',
+    '/cliente/login/reset': 'reset.html'
+  }
+
+  res.status(200).send(routes)
+}
